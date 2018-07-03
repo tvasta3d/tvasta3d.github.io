b5c2983731a219e84064ea52531acde29b3b0442
Athul Krishnan <atk007@gmail.com>
2018-07-03 18:46:13 +0530

    remove heavy video files

--- assets/js/main.js
+++
@@ -1,8 +0,0 @@
-// Navbar Color & Transparency
-$(window).scroll(function () {
-if ($(window).scrollTop() >= 50) {
-$('.navbar').css('background','#333333');
-} else {
-$('.navbar').css('background','transparent');
-}
-});
